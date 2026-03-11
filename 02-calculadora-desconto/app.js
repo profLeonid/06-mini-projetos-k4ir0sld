66c9a8b7f32e7da001a05369483e30e84b6ff7d0
lucasaleduarte <lucasaleduarte0@gmail.com>
2026-03-11 11:18:55 -0300

    Altera cor iniciado

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -1,11 +1,27 @@
 'use strict'
 
 function calcularDesconto(){
-    const precoOriginal = document.getElementById('precoOriginal')
-    const desconto      = document.getElementById('desconto')
+    const precoOriginal = parseFloat(document.getElementById('precoOriginal').value)
+    const desconto      = parseFloat(document.getElementById('desconto').value)
     const resultadoEconomizado     = document.getElementById('resultadoEconomizado')
     const resultadoFinal           = document.getElementById('resultadoFinal')
 
-    resultadoEconomizado = precoOriginal * (desconto / 100)
-    resultadoFinal = precoOriginal - resultadoEconomizado
+    const valorEconomizado = precoOriginal * (desconto / 100)
+    const precoFinal = precoOriginal - valorEconomizado
+
+    resultadoEconomizado.innerText = valorEconomizado.toFixed(2)
+    resultadoFinal.innerText       = precoFinal.toFixed(2)
+
+    trocaCorDesconto(desconto, resultadoFinal)
+
+}
+
+function trocaCorDesconto(desconto) {
+    const divResultado = document.querySelector('.resultado') // ✅ pega a div
+
+    if (desconto <= 5) {
+        divResultado.classList.add('resultado-verde')
+    } else {
+        divResultado.classList.remove('resultado-verde') // limpa se não for verde
+    }
 }
