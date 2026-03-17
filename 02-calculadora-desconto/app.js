1aeb7c31d8307423d48588927cb72c5ba711bf07
lucasaleduarte <lucasaleduarte0@gmail.com>
2026-03-17 09:29:45 -0300

    Calculadora Desconto Finalizado

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -1,27 +1,76 @@
 'use strict'
 
-function calcularDesconto(){
-    const precoOriginal = parseFloat(document.getElementById('precoOriginal').value)
-    const desconto      = parseFloat(document.getElementById('desconto').value)
-    const resultadoEconomizado     = document.getElementById('resultadoEconomizado')
-    const resultadoFinal           = document.getElementById('resultadoFinal')
+//MINHA PRIMEIRA TENTATIVA ANTES DO PROFESSOR CORRIGIR
+//A SAIDA E LOGICA DERAM CERTO, MAS AGORA VAMOS UTILIZAR FUNAÇÃO PURA
 
-    const valorEconomizado = precoOriginal * (desconto / 100)
-    const precoFinal = precoOriginal - valorEconomizado
+// function calcularDesconto(){
+//     const precoOriginal = parseFloat(document.getElementById('precoOriginal').value)
+//     const desconto      = parseFloat(document.getElementById('desconto').value)
+//     const resultadoEconomizado     = document.getElementById('resultadoEconomizado')
+//     const resultadoFinal           = document.getElementById('resultadoFinal')
 
-    resultadoEconomizado.innerText = valorEconomizado.toFixed(2)
-    resultadoFinal.innerText       = precoFinal.toFixed(2)
+//     const valorEconomizado = precoOriginal * (desconto / 100)
+//     const precoFinal = precoOriginal - valorEconomizado
 
-    trocaCorDesconto(desconto, resultadoFinal)
+//     resultadoEconomizado.innerText = valorEconomizado.toFixed(2)
+//     resultadoFinal.innerText       = precoFinal.toFixed(2)
 
+//     trocaCorDesconto(desconto, resultadoFinal)
+
+// }
+
+// function trocaCorDesconto(desconto) {
+//     const divResultado = document.querySelector('.resultado')
+
+//     if (desconto <= 5) {
+//         divResultado.classList.add('resultado-verde')
+//     } else {
+//         divResultado.classList.remove('resultado-verde') 
+//     }
+// }
+
+/*
+Função Pura (Responsabilidade Unica)
+    Ela usa parametros e entrega um return, iiso para que ela seja mais versátil e utilizada
+    em outros projetos, afinal essa função é pura exerce apenas uma coisa
+
+function calcularValorEconomizado(preco, desconto) {
+    return preco * desconto / 100
 }
 
-function trocaCorDesconto(desconto) {
-    const divResultado = document.querySelector('.resultado') // ✅ pega a div
+function calcularPrecoFinal (preco, valorEconomizado) {
+    return preco - valorEconomizado
+}
+*/
 
-    if (desconto <= 5) {
-        divResultado.classList.add('resultado-verde')
-    } else {
-        divResultado.classList.remove('resultado-verde') // limpa se não for verde
+//Função em seta - Arrow
+
+const calcularValorEconomizado = (preco, desconto) => preco * desconto / 100
+
+const calcularPrecoFinal = (preco, valorEconomizado) => preco - valorEconomizado
+
+function escolherCor (desconto) {
+    if(desconto <= 5){
+        return 'desconto1'
+    }else if (desconto <= 10) {
+        return 'desconto2'
+    }else{
+        return 'desconto3'
     }
+}
+
+function handleClick () {
+    const preco = Number(document.getElementById('precoOriginal').value)
+    const desconto = Number(document.getElementById('desconto').value)
+    const resultado = document.getElementById('resultado')
+
+    const valorEconomizado  = calcularValorEconomizado(preco, desconto)
+    const precoFinal        = calcularPrecoFinal(preco, valorEconomizado)
+
+    const cor = escolherCor(desconto)
+
+    resultado.textContent = `${valorEconomizado} - ${precoFinal}`
+    resultado.classList.remove('desconto1', 'desconto2', 'desconto3')
+    resultado.classList.add(cor)
+
 }
